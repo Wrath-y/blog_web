609bb399d84e2c189aa5e737a6a8b6cef6d13729
Gilgamesh <gilgamesh@ganguo.hk>
2018-11-10 19:28:45 +0800

    feat: 添加topbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,35 @@
 <template>
   <div>
+    <el-menu class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">文章</el-menu-item>
+      <el-menu-item index="2">图片</el-menu-item>
+    </el-menu>
     <nuxt/>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import {Menu, MenuItem} from 'element-ui';
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+export default {
+    components: {
+        [Menu.name]: Menu,
+        [MenuItem.name]: MenuItem,
+    },
+    data() {
+        return {
+		};
+    },
+    computed: {},
+    watch: {},
+    methods: {
+        handleSelect() {
+            this.$router.push('/articles');
+        },
+    },
+    mounted() {
+    },
+    beforeDestroy() {
+    },
+};
+</script>
