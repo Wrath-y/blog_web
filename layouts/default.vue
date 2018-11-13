dab25c921bc140c8aad9b40d5945c34694e064da
Gilgamesh <gilgamesh@ganguo.hk>
2018-11-13 21:02:32 +0800

    style: 优化样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
-    <el-menu class="el-menu font" mode="horizontal" @select="handleSelect">
-        <el-menu-item index="/">首页</el-menu-item>
-        <el-menu-item index="/articles">文章</el-menu-item>
-        <el-menu-item index="/time-series">归档</el-menu-item>
-        <el-submenu show-timeout='100' hide-timeout='200' index="/lists">
-            <template slot="title">清单</template>
+    <div>
+        <div class="logo"></div>
+        <el-menu router class="el-menu navbar" mode="horizontal">
+            <el-menu-item index="/">首页</el-menu-item>
+            <el-menu-item index="/articles">文章</el-menu-item>
+            <el-menu-item index="/time-series">归档</el-menu-item>
+            <el-submenu :show-timeout='100' :hide-timeout='100' index="/lists">
+                <template slot="title">清单</template>
                 <el-menu-item index="/lists/musics">歌单</el-menu-item>
                 <el-menu-item index="/lists/pixivs">图片</el-menu-item>
             </el-submenu>
-        </el-submenu>
-        <el-menu-item index="/comments">留言板</el-menu-item>
-        <el-menu-item index="/friends">友人帐</el-menu-item>
-        <el-menu-item index="/abouts">关于</el-menu-item>
-    </el-menu>
-    <nuxt/>
-  </div>
+            <el-menu-item index="/comments">留言板</el-menu-item>
+            <el-menu-item index="/friends">友人帐</el-menu-item>
+            <el-menu-item index="/abouts">关于</el-menu-item>
+        </el-menu>
+        <nuxt/>
+    </div>
 </template>
 
 <script>
@@ -33,11 +33,7 @@ export default {
     },
     computed: {},
     watch: {},
-    methods: {
-        handleSelect(key) {
-            this.$router.push(key);
-        },
-    },
+    methods: {},
     mounted() {
     },
     beforeDestroy() {
@@ -46,7 +42,8 @@ export default {
 </script>
 
 <style>
-.font {
+.navbar {
     font-family: 'Source Han Serif SC','Source Han Serif','source-han-serif-sc','PT Serif','SongTi SC','MicroSoft Yahei',Georgia,serif;
+    padding-left: 36%;
 }
 </style>
