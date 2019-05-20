3f3f6d45fb8634bddecd23e215c6f95fd986c700
gilgamesh-y <805655528@qq.com>
2019-05-20 14:59:25 +0800

    add title

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -2,6 +2,23 @@
     <div class="wrap">
         <div class="top"></div>
         <el-card v-loading="loading">
+            <div class="header">
+                <h1>{{form.title}}</h1>
+                <div class="post-meta">
+                    <span>
+                        <i class="el-icon-view"></i>
+                        {{form.hits}}热度
+                    </span>
+                    <span style="margin: 0 10px">
+                        <i class="el-icon-edit"></i>
+                        {{form.commentCount || 0}}条评论
+                    </span>
+                    <span class="tags">
+                        <i class="el-icon-document"></i>
+                        {{form.tags}}
+                    </span>
+                </div>
+            </div>
             <div v-html="form.html"></div>
         </el-card>
         <Comment />
@@ -59,6 +76,20 @@ export default {
     margin: auto;
     .top {
         padding-top: 81px;
+    }
+    .header {
+        margin-bottom: 20px;
+        text-align: center;
+        .post-meta {
+            margin-bottom: 15px;
+            width: 100%;
+            color: #888;
+            font-size: 12px;
+            .tags:hover {
+                color: #e67474;
+                cursor: url(https://gilgamesh-10047150.cos.ap-shanghai.myqcloud.com/ayuda.cur),auto;
+            }
+        }
     }
 }
 </style>
