d81a460127ad2fa0fcede04f5b3bb5d8b236f112
Wrath-y <805655528@qq.com>
2023-02-09 16:46:14 +0800

    feat: 更新api

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,8 +55,7 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: 'https://wrath.cc/go/',
-    // baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://wrath.cc/go/api/',
   },
 
   /*
